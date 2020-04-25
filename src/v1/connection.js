7e41532dbf1e6ee0d5f2434217b224ed9fc879fb
Takuto Yanagida <yanagida@stxst.com>
2020-04-25 17:17:28 +0900

    Improvement

--- src/v1/connection.js
+++ src/v1/connection.js
@@ -1,69 +1,81 @@
+/**
+ * Connection - Wemote
+ * @author Takuto Yanagida
+ * @version 2020-04-25
+ */
 
-(function () {
+
+window.WEMOTE = window['WEMOTE'] || {};
+
+
+(function (NS) {
+	const URL_SIGNALING = 'wss://ayame-lite.shiguredo.jp/signaling';
+
 	function loadScript(src) {
 		const s = document.createElement('script');
 		s.src = src;
 		document.head.appendChild(s);
 	}
 	loadScript('https://laccolla.com/api/wemote/v1/lib/ayame.min.js');
-})();
 
-const URL_SIGNALING = 'wss://ayame-lite.shiguredo.jp/signaling';
+	class Connection {
 
-class Connection {
+		constructor(roomId, onMessage = null, onStateChange = null) {
+			this._roomId  = roomId;
+			this._onMsg   = onMessage;
+			this._onState = onStateChange;
 
-	constructor(roomId, onMessage = null, onStateChange = null) {
-		this._roomId  = roomId;
-		this._onMsg   = onMessage;
-		this._onState = onStateChange;
-
-		this._opts  = Ayame.defaultOptions;
-		this._label = 'dataChannel';
-		this._con   = null;
-		this._ch    = null;
-	}
-
-	async start() {
-		this._con = Ayame.connection(URL_SIGNALING, this._roomId, this._opts, true);
-		this._con.on('open', async (e) => {
-			this._ch = await this._con.createDataChannel(this._label);
-			if (this._ch) {
-				this._ch.onmessage = (e) => { this.receive(e) };
-			}
-			if (this._onState) this._onState('open');
-		});
-		this._con.on('connect', (e) => {
-			if (this._onState) this._onState('connect');
-		});
-		this._con.on('datachannel', (ch) => {
-			if (!this._ch) {
-				this._ch = ch;
-				this._ch.onmessage = (e) => { this.receive(e) };
-			}
-		});
-		this._con.on('disconnect', (e) => {
-			this._con = null;
-			this._ch = null;
-			if (this._onState) this._onState('disconnect');
-		});
-		this._con.on('bye', (e) => {
-			if (this._onState) this._onState('bye');
-		});
-		await this._con.connect(null).catch((e) => {console.log(e);});
-	}
-
-	stop() {
-		if (this._con) this._con.disconnect();
-	}
-
-	send(data) {
-		if (this._ch && this._ch.readyState === 'open') {
-			this._ch.send(data);
+			this._opts  = Ayame.defaultOptions;
+			this._label = 'dataChannel';
+			this._con   = null;
+			this._ch    = null;
 		}
+
+		async start() {
+			this._con = Ayame.connection(URL_SIGNALING, this._roomId, this._opts, true);
+			this._con.on('open', async (e) => {
+				this._ch = await this._con.createDataChannel(this._label);
+				if (this._ch) {
+					this._ch.onmessage = (e) => { this.receive(e) };
+				}
+				if (this._onState) this._onState('open', e);
+			});
+			this._con.on('connect', (e) => {
+				if (this._onState) this._onState('connect', e);
+			});
+			this._con.on('datachannel', (ch) => {
+				if (!this._ch) {
+					this._ch = ch;
+					this._ch.onmessage = (e) => { this.receive(e) };
+				}
+			});
+			this._con.on('disconnect', (e) => {
+				this._con = null;
+				this._ch = null;
+				if (this._onState) this._onState('disconnect', e);
+			});
+			this._con.on('bye', (e) => {
+				if (this._onState) this._onState('bye', e);
+			});
+			await this._con.connect(null).catch((e) => {console.log(e);});
+		}
+
+		stop() {
+			if (this._con) this._con.disconnect();
+		}
+
+		send(data) {
+			if (this._ch && this._ch.readyState === 'open') {
+				this._ch.send(data);
+			}
+		}
+
+		receive(e) {
+			if (this._onMsg) this._onMsg(e.data);
+		}
+
 	}
 
-	receive(e) {
-		if (this._onMsg) this._onMsg(e.data);
-	}
+	NS.Connection = Connection;
 
-}
+})(window.WEMOTE);
--- src/v1/receiver.js
+++ src/v1/receiver.js
@@ -1,11 +1,17 @@
 /**
- * Wemote Receiver
+ * Receiver - Wemote
  * @author Takuto Yanagida
  * @version 2020-04-25
  */
 
 
-const WEMOTE_RECEIVER = (function () {
+window.WEMOTE = window['WEMOTE'] || {};
+
+
+(function (NS) {
+	const URL_CONTROLLER = 'https://laccolla.com/api/wemote/v1/controller/';
+	const ROOMID_PREFIX  = 'wemote-';
+	const HASH_LENGTH    = 16;
 
 	function loadScript(src) {
 		const s = document.createElement('script');
@@ -15,10 +21,6 @@ const WEMOTE_RECEIVER = (function () {
 	loadScript('https://laccolla.com/api/wemote/v1/lib/qrcode.min.js');
 	loadScript('https://laccolla.com/api/wemote/v1/connection.min.js');
 
-	const HASH_LENGTH    = 16;
-	const URL_CONTROLLER = 'https://laccolla.com/api/wemote/v1/controller';
-	const ROOMID_PREFIX  = 'wemote-';
-
 	function createHash() {
 		return btoa(String.fromCharCode(...crypto.getRandomValues(new Uint8Array(HASH_LENGTH)))).substring(0, HASH_LENGTH);
 	}
@@ -26,8 +28,8 @@ const WEMOTE_RECEIVER = (function () {
 	function createQrCode(parent, hash) {
 		const qr = new QRCode(parent, {
 			text        : URL_CONTROLLER + '?' + hash,
-			width       : 128,
-			height      : 128,
+			width       : 256,
+			height      : 256,
 			correctLevel: QRCode.CorrectLevel.H
 		});
 		return qr;
@@ -36,22 +38,17 @@ const WEMOTE_RECEIVER = (function () {
 	let con = null;
 
 	function start(hash, onMessage, onStageChange) {
-		con = new Connection(ROOMID_PREFIX + hash, onMessage, onStageChange);
+		if (con) stop();
+		con = new WEMOTE.Connection(ROOMID_PREFIX + hash, onMessage, onStageChange);
 		setTimeout(() => { con.start(); }, 10);
 	}
 
 	function stop() {
-		if (con) {
-			con.stop();
-			con = null;
-		}
+		if (!con) return;
+		con.stop();
+		con = null;
 	}
 
-	return {
-		createHash,
-		createQrCode,
-		start,
-		stop,
-	};
+	NS.RECEIVER = { createHash, createQrCode, start, stop };
 
-}());
+}(window.WEMOTE));
